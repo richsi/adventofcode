5174785760fe14e293252e60cfbae99603528dfb
richsi <rzhsieh2013@gmail.com>
2023-12-05 23:40:26 -0800

    cleand up da6

--- 2023/day6/6.py
+++ 2023/day6/6.py
@@ -1,22 +1,16 @@
-lines = []
 with open("input.txt", "r") as file:
-    while line := file.readline():
-        lines.append(line.strip())
+    times = [t for t in file.readline().strip().split("Time: ")[1].split(" ") if t]
+    distances = [d for d in file.readline().strip().split("Distance: ")[1].split(" ") if d]
 
-times = [t for t in lines[0].split("Time: ")[1].split(" ") if t]
-distances = [d for d in lines[1].split("Distance: ")[1].split(" ") if d]
-
-res = 1
+res = []
 for time in range(len(times)):
     wins = 0
-    current_time = int(times[time]) #7
-    for t in range(current_time):
-        remaining_time = current_time - t - 1 #6
-        distance = remaining_time * (t+1) 
-        if distance > int(distances[time]):
+    for t in range(int(times[time])):
+        remaining_time = int(times[time]) - t - 1 #6
+        if remaining_time * (t+1)  > int(distances[time]):
             wins += 1
-    res *= wins
+    res.append(wins)
 
-print(res)
+print(res[0] * res[1] * res[2] * res[3])
 
 
--- 2023/day6/62.py
+++ 2023/day6/62.py
@@ -1,19 +1,10 @@
-lines = []
 with open("input.txt", "r") as file:
-    while line := file.readline():
-        lines.append(line.strip())
-
-#['Time:        44     89     96     91', 'Distance:   277   1136   1890   1768']
-times = "".join([t for t in lines[0].split("Time: ")[1].split(" ") if t])
-distances = "".join([d for d in lines[1].split("Distance: ")[1].split(" ") if d])
-
-print(times, distances)
+    times = "".join([t for t in file.readline().split("Time: ")[1].split(" ") if t])
+    distances = "".join([d for d in file.readline().split("Distance: ")[1].split(" ") if d])
 
 wins = 0
 for time in range(int(times)): 
-    remaining_time = int(times) - time - 1 
-    distance = remaining_time * (time + 1) 
-    print(distance)
+    distance = int(times) - time - 1  * (time + 1) 
     if distance > int(distances):
         wins += 1
 print(wins)
